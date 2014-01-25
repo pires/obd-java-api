ab307a0607da25dc785a3a60b54bca605a7d9d76
Paulo Pires <pjpires@gmail.com>
2014-01-25 14:45:21 +0000

    Removed some unused code.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

--- src/main/java/pt/lighthouselabs/obd/commands/protocol/ObdResetCommand.java
+++ src/main/java/pt/lighthouselabs/obd/commands/protocol/ObdResetCommand.java
@@ -12,9 +12,6 @@
  */
 package pt.lighthouselabs.obd.commands.protocol;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 /**
  * This method will reset the OBD connection.
  */
@@ -29,23 +26,6 @@ public class ObdResetCommand extends ObdProtocolCommand {
    */
   public ObdResetCommand(ObdResetCommand other) {
     super(other);
-  }
-
-  /**
-   * Reset command returns an empty string, so we must override the following
-   * two methods.
-   * 
-   * @throws IOException
-   */
-  @Override
-  public void readResult(InputStream in) throws IOException {
-    // do nothing
-    return;
-  }
-
-  @Override
-  public String getResult() {
-    return "";
   }
 
   @Override
