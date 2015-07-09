168380a0847c841c3a6678ed377417f4f3e6603c
Paulo Pires <pjpires@gmail.com>
2015-07-10 00:00:51 +0100

    Moved package to com.github.pires.obd. Fixes #36

---
+++ src/main/java/com/github/pires/obd/commands/engine/ThrottlePositionObdCommand.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.github.pires.obd.commands.engine;
+
+import com.github.pires.obd.commands.PercentageObdCommand;
+import com.github.pires.obd.enums.AvailableCommandNames;
+
+/**
+ * Read the throttle position in percentage.
+ */
+public class ThrottlePositionObdCommand extends PercentageObdCommand {
+
+  /**
+   * Default ctor.
+   */
+  public ThrottlePositionObdCommand() {
+    super("01 11");
+  }
+
+  /**
+   * Copy ctor.
+   *
+   * @param other a {@link com.github.pires.obd.commands.engine.ThrottlePositionObdCommand} object.
+   */
+  public ThrottlePositionObdCommand(ThrottlePositionObdCommand other) {
+    super(other);
+  }
+
+  /**
+	 * 
+	 */
+  @Override
+  public String getName() {
+    return AvailableCommandNames.THROTTLE_POS.getValue();
+  }
+
+}
---
+++ src/main/java/com/github/pires/obd/commands/protocol/AvailablePidsObdCommand.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.github.pires.obd.commands.protocol;
+
+import com.github.pires.obd.commands.PersistentObdCommand;
+import com.github.pires.obd.enums.AvailableCommandNames;
+
+public class AvailablePidsObdCommand extends PersistentObdCommand {
+
+  /**
+   * Default ctor.
+   */
+  public AvailablePidsObdCommand() {
+    super("01 00");
+  }
+
+  /**
+   * Copy ctor.
+   *
+   * @param other a {@link AvailablePidsObdCommand} object.
+   */
+  public AvailablePidsObdCommand(AvailablePidsObdCommand other) {
+    super(other);
+  }
+
+  @Override
+  protected void performCalculations() {
+
+  }
+
+  @Override
+  public String getFormattedResult() {
+    return String.valueOf(rawData);
+  }
+
+  @Override
+  public String getName() {
+    return AvailableCommandNames.PIDS.getValue();
+  }
+
+}
