e8b2a809aed7c76cd6bf637a815acbe471d1b7c3
Andreas <andreas@eevol.lu>
2015-07-16 20:45:57 +0200

    added command

---
+++ src/main/java/com/github/pires/obd/commands/engine/AbsoluteLoadObdCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 Andreas.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pires.obd.commands.engine;
+
+import com.github.pires.obd.commands.PercentageObdCommand;
+import com.github.pires.obd.enums.AvailableCommandNames;
+
+/**
+ *
+ * @author Andreas
+ */
+public class AbsoluteLoadObdCommand extends PercentageObdCommand {
+
+  /**
+   * Default ctor.
+   */
+  public AbsoluteLoadObdCommand() {
+    super("01 43");
+  }
+
+  /**
+   * Copy ctor.
+   *
+   * @param other a {@link AbsoluteLoadObdCommand} object.
+   */
+  public AbsoluteLoadObdCommand(AbsoluteLoadObdCommand other) {
+    super(other);
+  }
+
+  @Override
+  protected void performCalculations() {
+    // ignore first two bytes [hh hh] of the response
+    int a = buffer.get(2);
+    int b = buffer.get(3);
+    percentage = (a * 256 + b) * 100 / 255;
+  }
+
+  /**
+   * @return a double.
+   */
+  public double getRatio() {
+    return percentage;
+  }
+
+  @Override
+  public String getName() {
+    return AvailableCommandNames.ABS_LOAD.getValue();
+  }
+
+}
