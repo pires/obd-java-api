34c084ce976d1f0adde639538dcbb5928f2a6a45
Pho3niX90 <shaun@jvn.sx>
2015-09-15 15:19:07 +0200

    Update SpacesOffCommandTest.java

--- src/test/java/com/github/pires/obd/commands/SpacesOffCommandTest.java
+++ src/test/java/com/github/pires/obd/commands/SpacesOffCommandTest.java
@@ -2,9 +2,6 @@ package com.github.pires.obd.commands.protocol;
 
 import junit.framework.TestCase;
 
-/**
- * Created by sjanvan1 on 2015/09/15.
- */
 public class SpacesOffCommandTest extends TestCase {
 
     public void testGetFormattedResult() throws Exception {
