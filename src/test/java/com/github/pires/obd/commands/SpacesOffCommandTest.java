5b3dae90936a279a0c6ca708e2b883562683b8e7
Pho3niX90 <shaun@jvn.sx>
2015-09-15 14:21:14 +0200

    Create SpacesOffCommandTest.java

---
+++ src/test/java/com/github/pires/obd/commands/SpacesOffCommandTest.java
@@ -0,0 +1,17 @@
+package com.github.pires.obd.commands.protocol;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by sjanvan1 on 2015/09/15.
+ */
+public class SpacesOffCommandTest extends TestCase {
+
+    public void testGetFormattedResult() throws Exception {
+
+    }
+
+    public void testGetName() throws Exception {
+
+    }
+}
