7e55e7d65ea161d1be9cf28734c32721daafd370
Héctor Barreras Almarcha <hbaelx@gmail.com>
2015-10-23 12:23:49 +0200

    Fixed bug mistaking last command of each byte for the first in the following More tests added

--- src/test/java/com/github/pires/obd/utils/CommandAvailabilityHelperTest.java
+++ src/test/java/com/github/pires/obd/utils/CommandAvailabilityHelperTest.java
@@ -46,10 +46,31 @@ public class CommandAvailabilityHelperTest {
         assertEquals(CommandAvailabilityHelper.isAvailable("00", "BE1FA813"), true);
         assertEquals(CommandAvailabilityHelper.isAvailable("02", "BE1FA813"), false);
         assertEquals(CommandAvailabilityHelper.isAvailable("07", "BE1FA813"), true);
-        assertEquals(CommandAvailabilityHelper.isAvailable(new ThrottlePositionCommand().getCommandPID() /*11*/, "BE1FA813"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("11", "BE1FA813"), true);
         assertEquals(CommandAvailabilityHelper.isAvailable("1A", "BE1FA813"), false);
         assertEquals(CommandAvailabilityHelper.isAvailable("1D", "BE1FA813"), false);
         assertEquals(CommandAvailabilityHelper.isAvailable("1F", "BE1FA813"), true);
+
+        assertEquals(CommandAvailabilityHelper.isAvailable("20", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("1F", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("1D", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("1E", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("1C", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("1B", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("1A", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("19", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("18", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("17", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("16", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("15", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("14", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("13", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("12", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("11", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("10", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("0F", "983BA013"), true);
+        assertEquals(CommandAvailabilityHelper.isAvailable("0E", "983BA013"), false);
+        assertEquals(CommandAvailabilityHelper.isAvailable("0D", "983BA013"), true);
 
         try {
             CommandAvailabilityHelper.isAvailable("2F", "BE1FA813");
