6a21776c8e8a1070ead90a82a50b54927635a92b
Pho3niX90 <shaun@jvn.sx>
2016-05-11 15:03:56 +0200

    Added additional algorithm for VINs returning special chars
    Extra tests: http://goo.gl/QuLlY6
    Possible fix for: https://github.com/pires/android-obd-reader/issues/174

--- src/main/java/com/github/pires/obd/commands/control/VinCommand.java
+++ src/main/java/com/github/pires/obd/commands/control/VinCommand.java
@@ -2,6 +2,8 @@ package com.github.pires.obd.commands.control;
 
 import com.github.pires.obd.commands.PersistentCommand;
 import com.github.pires.obd.enums.AvailableCommandNames;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VinCommand extends PersistentCommand {
 
@@ -32,6 +34,12 @@ public class VinCommand extends PersistentCommand {
         String workingData;
         if (result.contains(":")) {//CAN(ISO-15765) protocol.
             workingData = result.replaceAll(".:", "").substring(9);//9 is xxx490201, xxx is bytes of information to follow.
+            Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+            Matcher m = p.matcher(convertHexToString(workingData));
+            if(m.find())
+            {
+                workingData = result.replaceAll("0:49", "").replaceAll(".:","");
+            }
         } else {//ISO9141-2, KWP2000 Fast and KWP2000 5Kbps (ISO15031) protocols.
             workingData = result.replaceAll("49020.", "");
         }
