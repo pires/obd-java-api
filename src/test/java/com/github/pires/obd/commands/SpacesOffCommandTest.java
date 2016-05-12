1702a6d9e7ebc31f0cf44c1ff426e8d079fb786d
Pho3niX90 <shaun@jvn.sx>
2016-05-12 16:30:43 +0200

    Create SpacesOffCommandTest.java

---
+++ src/test/java/com/github/pires/obd/commands/SpacesOffCommandTest.java
@@ -0,0 +1,50 @@
+package com.github.pires.obd.exceptions;
+
+import com.github.pires.obd.commands.protocol.SpacesOffCommand;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.powermock.api.easymock.PowerMock.*;
+
+/**
+ * Test results with spacesoff command.
+ */
+@PrepareForTest(InputStream.class)
+public class SpacesOffCommandTest {
+
+    private SpacesOffCommand command;
+    private InputStream mockIn;
+
+    /**
+     * @throws Exception
+     */
+    @BeforeMethod
+    public void setUp() throws Exception {
+        command = new SpacesOffCommand();
+    }
+
+    /**
+     * @throws java.io.IOException, java.lang.InterruptedException
+     */
+    @Test(expectedExceptions = NoDataException.class)
+    public void testValidSpeedMetricWithMessage() throws IOException, InterruptedException {
+        // mock InputStream read
+        mockIn = createMock(InputStream.class);
+        mockIn.read();
+        expectLastCall().andReturn((byte) 'O');
+        expectLastCall().andReturn((byte) 'K');
+
+        replayAll();
+
+        // call the method to test
+        command.run(mockIn, new ByteArrayOutputStream());
+
+        verifyAll();
+    }
+
+}
